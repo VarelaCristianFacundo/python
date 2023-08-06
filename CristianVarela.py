17e2fa3b6436f3de1352a23ee246081446cd6e1b
VarelaCristianFacundo <cvarelagarcia@gmail.com>
2023-08-06 14:27:09 -0300

    login y guardo informacion

--- CristianVarela.py
+++ CristianVarela.py
@@ -45,3 +45,59 @@ def existeArchivo():
         return False
 
 # Funcion para almacenar la informacion
+def almacenarInformacion():
+    try:
+        # Valida si existe el archivo
+        if existeArchivo() == False:
+            crearArchivo()
+        # Lee el archivo
+        data = eval(leerArchivo())
+        id = int(len(data)) + 1 
+        nombre = input("Ingrese su nombre: ")
+        user = input("Ingrese su usuario: ")
+        password = input("Ingrese su contraseña: ")
+        data.append({"id": id, "nombre": nombre, "user": user, "password": password})
+        guardarInformacion(data)
+    except Exception as e:
+        print("Error al almacenar la informacion " +  e.__str__() )
+        
+
+# Escribe en la funcion 
+def guardarInformacion(data):
+    file = open("data.txt", "w")
+    file.write(str(data))
+    file.close()
+    print("Informacion guardada con exito")
+
+#Login de usuario
+def login():
+    try:
+        # Valida si existe el archivo
+        if existeArchivo() == False:
+            crearArchivo()
+        # Lee el archivo
+        data = eval(leerArchivo())
+        user = input("Ingrese su usuario: ")
+        password = input("Ingrese su contraseña: ")
+        for i in data:
+            if i["user"] == user and i["password"] == password:
+                print("Bienvenido " + i["nombre"])
+                break
+            else:
+                print("Usuario o contraseña incorrectos")
+        else:
+            print("No existe datos favor de crear el archivo")
+    except Exception as e:
+        print("Error al almacenar la informacion " +  e.__str__() )
+
+
+
+
+# Funcion para validar el usuario
+select = input('Seleccione una opcion: \n 1. Almacenar informacion \n 2. Login \n 3. Salir \n')
+if(select == "1"):
+    almacenarInformacion()
+elif(select == "2"):
+    login()
+elif(select == "3"):
+    exit()
