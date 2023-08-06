250678cd48ea8b0e9a3c4e995b242aac51ab5a64
VarelaCristianFacundo <cvarelagarcia@gmail.com>
2023-08-06 14:10:19 -0300

    consigna

---
+++ CristianVarela.py
@@ -0,0 +1,20 @@
+# Objetivo
+# Practicar el concepto de funciones. Preparar la parte lógica para el registro de usuarios.
+
+# Consigna
+
+# Crear un programa que permita emular el registro y almacenamiento de usuarios en una base de datos. Crear el programa utilizando el concepto de funciones, diccionarios, bucles y condicionales.
+# Formato
+
+# El proyecto debe compartirse utilizando Colab bajo el nombre “Primera pre-entrega+Apellido”.
+# Se debe entregar
+# Se debe entregar todo el programa.
+
+# Sugerencias
+# El formato de registro es: Nombre de usuario y Contraseña.
+# Utilizar una función para almacenar la información y otra función para mostrar la información.
+# Utilizar un diccionario para almacenar dicha información, con el par usuario-contraseña (clave-valor).
+# Utilizar otra función para el login de usuarios, comprobando que la contraseña coincida con el usuario.
+# Adicional
+# Utilizando los conceptos de la clase 8, guarde la información en un archivo de texto dentro de su Drive."
+
