4855da809f9ab9b2c3bd16d963c11d871a92f7dd
VarelaCristianFacundo <cvarelagarcia@gmail.com>
2023-08-17 19:58:46 -0300

    creo el setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+# Debe ir en la carpeta anterior
+
+from setuptools import setup
+
+setup(
+    name = 'segunda_pre-entrega_Cristian_Varela',
+    version = '1.0',
+    description = 'Segunda pre-entrega del curso de coder python',
+    author = 'Cristian Varela',
+    author_email = 'cvarelagarcia@gmail.com',
+    packages = ['PrimeraEntrega','SegundaEntrega','SegundaEntrega/clases']
+)
