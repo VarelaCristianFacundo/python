faa44e6f93651fdee202a26f4c05f9cd87487108
VarelaCristianFacundo <cvarelagarcia@gmail.com>
2023-08-17 19:38:46 -0300

    Creo el main desde donde importo la clase

---
+++ SegundaEntrega/main.py
@@ -0,0 +1,14 @@
+from clases.clases import Cliente, Producto_dulce
+
+# Generar Cliente
+cliente1 = Cliente('33.445.227','Cristian','Varela','Masculino','32','Argentina')
+cliente2 = Cliente('55.332.212','Mauricio','Cuello','Masculino','32','Argentina')
+cliente3 = Cliente('14.690.122','Rodrigo','Diaz','Masculino','40','Chile')
+
+# Generar Productos
+producto_dulce1 = Producto_dulce(1,'Chupetin','Arcor',8.59,'Masticable',15)
+producto_dulce2 = Producto_dulce(2,'Caramelo','Arcor',3.59,'Masticable',10)
+producto_dulce3 = Producto_dulce(3,'Chocolate','Ferrero',5.59,'Amargo',7)
+producto_dulce4 = Producto_dulce(4,'Alfajor','Ferrero',4.99,'Dulce',3)
+
+#############################################################################################################
