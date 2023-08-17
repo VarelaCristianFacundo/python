705fdcb87ab245c794468f859c9ac6346190aba8
VarelaCristianFacundo <cvarelagarcia@gmail.com>
2023-08-17 19:26:27 -0300

    archivo clases, con clase cliente

---
+++ SegundaEntrega/clases/clases.py
@@ -0,0 +1,11 @@
+# CLASE CLIENTE
+class Cliente():
+    def __init__(self, dni, nombre, apellido, sexo, edad, pais):
+        self.dni = dni
+        self.nombre = nombre
+        self.apellido = apellido
+        self.sexo = sexo
+        self.edad = edad
+        self.pais = pais
+        self.monto_total = 0
+        
