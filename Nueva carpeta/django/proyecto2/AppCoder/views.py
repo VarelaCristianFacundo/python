489f42d609ce6f1a17078248c2973b4a6a3362b9
VarelaCristianFacundo <cvarelagarcia@gmail.com>
2023-08-25 18:21:47 -0300

    new project with query to sqlite db

--- Nueva carpeta/django/proyecto2/AppCoder/views.py
+++ Nueva carpeta/django/proyecto2/AppCoder/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Curso
 
 # Create your views here.
+def curso(req, nombre, camada):
+    
+    curso = Curso(nombre=nombre, camada=camada)
+    curso.save()
+    
+    return HttpResponse(f"""
+                        <p>Curso: {curso.nombre} - Camada: {curso.camada} Creado con éxito</p>                        
+                        """)
+    
+def listar_cursos(req):
+    lista = Curso.objects.all()
+    
+    return render(req, "lista_cursos.html", {"lista_cursos": lista})
--- Nueva carpeta/django/proyecto2/proyecto2/urls.py
+++ Nueva carpeta/django/proyecto2/proyecto2/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from AppCoder.views import curso, listar_cursos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('agrega-curso/<nombre>/<camada>', curso),
+    path('lista-cursos/', listar_cursos),
 ]
