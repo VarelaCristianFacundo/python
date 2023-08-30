f5347aedacb2f484c24fcb0439f158bf4c7360b1
VarelaCristianFacundo <cvarelagarcia@gmail.com>
2023-08-29 22:01:29 -0300

    agrego diferentes pages y urls en django

--- django/proyecto2/proyecto2/proyecto2/urls.py
+++ django/proyecto2/proyecto2/proyecto2/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from AppCoder.views import curso, listar_cursos
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('agrega-curso/<nombre>/<camada>', curso),
-    path('lista-cursos/', listar_cursos),
+    path('app-coder/', include('AppCoder.urls')),
 ]
