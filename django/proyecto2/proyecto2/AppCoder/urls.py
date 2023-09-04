369c182b00495d021dd85e27a077e135f9cc629c
VarelaCristianFacundo <cvarelagarcia@gmail.com>
2023-09-04 16:50:00 -0300

    agrego herencia de templates

--- django/proyecto2/proyecto2/AppCoder/urls.py
+++ django/proyecto2/proyecto2/AppCoder/urls.py
@@ -4,10 +4,10 @@ from .views import *
 urlpatterns = [
     path('agrega-curso/<nombre>/<camada>', curso),
     path('lista-cursos/', listar_cursos),
-    path('', inicio),
-    path('cursos/', cursos),
-    path('profesores/', profesores),
-    path('estudiantes/', estudiantes),
-    path('entregables/', entregables),    
-    path('login/', login),    
+    path('', inicio, name="Inicio"),
+    path('cursos/', cursos, name="Cursos"),
+    path('profesores/', profesores, name="Profesores"),
+    path('estudiantes/', estudiantes, name="Estudiantes"),
+    path('entregables/', entregables, name="Entregables"),    
+    path('login/', login, name="Login"),    
 ]
